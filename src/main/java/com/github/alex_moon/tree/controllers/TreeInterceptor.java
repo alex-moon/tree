919dc80b3f566d9482ebb6c2f16d0db23cac25a0
Alex Moon <alex.moon@printed.com>
2015-11-01 21:20:56 +0000

    X-Api-Key header authentication (is this really the best way?)

--- src/main/java/com/github/alex_moon/tree/controllers/TreeInterceptor.java
+++ src/main/java/com/github/alex_moon/tree/controllers/TreeInterceptor.java
@@ -10,6 +10,21 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class TreeInterceptor extends HandlerInterceptorAdapter {
     @Override
+    public boolean preHandle(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        Object handler
+    ) throws Exception {
+        String apiKey = request.getHeader("X-Api-Key");
+        // @todo pull it from the DB might be nice
+        if (apiKey == null || !apiKey.toString().equals("123")) {
+            response.setStatus(403);
+            return false;
+        }
+        return super.preHandle(request, response, handler);
+    }
+
+    @Override
     public void postHandle(
         final HttpServletRequest request,
         final HttpServletResponse response,
